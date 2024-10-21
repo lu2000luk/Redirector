bfa06f5246a121888696acbc4e0ccc788aa9f3c8
lu2000luk <lu2000luk@gmail.com>
2024-10-21 23:10:35 +0200

    Epilepsy backgrounds go brrr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,8 @@
     <title>{loadingState}</title>
 </svelte:head>
 
-<div class="w-full h-full flex justify-center items-center bg-slate-950">
-    <div class="py-5 px-10 shadow-xl rounded-xl bg-slate-800 flex justify-center text-white text-xl">
+<div class="w-full h-full flex justify-center items-center">
+    <div class="py-12 cursor-pointer px-10 shadow-xl rounded-xl card flex justify-center text-white text-xl">
         {loadingState}
     </div>
 </div>
